1167abaa26581580e8dfe18eeaaf1fbf1fdaaa4a
Debamitra Mukherjee <debamitra.mukherjee@gmail.com>
2025-01-11 16:32:47 +0530

    find peak

--- leetcode-150/main.py
+++ leetcode-150/main.py
@@ -56,3 +56,29 @@ def searchMatrix(self, matrix, target):
 
         
 searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]],3)
+
+
+##########################################################
+
+# 162. Find Peak Element
+# MEDIUM
+
+# Non Binary search solution - O(N), optimize for O(log N)
+def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        size = len(nums)
+        if size == 1:
+            return 0
+        if size == 2:
+            return nums.index(max(nums[0],nums[1]))
+        for i in range(size):
+            if i == 0 and nums[i+1] < nums[i]:
+                return i
+            if i == size - 1  and nums[i-1] < nums[i]:
+                return i
+            if nums[i-1] < nums[i] and nums[i+1] < nums[i]:
+                return i
+        return -1
